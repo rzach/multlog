796037d545a3d53dc2f07284e13682f98ae56dfa
Gernot Salzer <salzer@logic.at>
2021-05-09 13:22:56 +0200

    Caching of minimized operators added

--- ml/ml.pl
+++ ml/ml.pl
@@ -5,6 +5,8 @@
 %%% Interface:
 %%%            lgc2tex(In,Out,Cnf). Translates LGC-file "In" to TeX-file "Out"
 %%%                                 using configuration in "Cnf"
+%%%
+%%% 9.5.2021: Caching added
 
 :- ['@mlpath@/ml_conf'].
 :- ['@mlpath@/ml_util'].
@@ -17,6 +19,8 @@
 :- ['@mlpath@/ml_minimize'].
 :- ['@mlpath@/ml_compactrepr'].
 
+:- dynamic cached/1.
+
 lgc2tex(In, Out, CFN) :-
 	init_errors,
 	println,
@@ -27,6 +31,25 @@ lgc2tex(In, Out, CFN) :-
 	  ,  println_terms([' using configuration in ', CFN])
 	),
 	lgc_in(In),
+	atom_concat(In, '.cac', Cache),
+	cache_in(Cache),
 	check,
 	kernel,
+	cache_out(Cache),
 	tex_out(Out, CFN).
+
+cache_in(Cache) :-
+	(exists_file(Cache) ->
+	   [Cache]
+	;  true
+	).
+
+cache_out(Cache) :-
+	tell(Cache),
+	(cached(T),
+	   write_canonical(cached(T)),
+	   write('.'), nl,
+	   fail
+	;  true
+	),
+	told.
