4a3de19db3299f288cc619bd19d7ce86e6529381
gsalzer <salzer@logic.at>
2021-05-20 16:57:09 -0600

    generate generic formulas of a logic

--- ml/ml_interactive.pl
+++ ml/ml_interactive.pl
@@ -1,13 +1,29 @@
 %% ml_interactive.pl -- Run MUltlog interactively
+multlog.
 
-:- ['@mlpath@/ml'].
+user:file_search_path(multlog, Dir) :-
+    source_file(multlog, File),
+    file_directory_name(File, Dir).
 
-:- dynamic logName/2, logTVs/2, logDTVs/2, logNDTVs/2, logOp/3.
+:- set_prolog_flag(double_quotes, codes).
+
+% Load required files
+:- [multlog(ml_conf)].
+:- [multlog(ml_util)].
+:- [multlog(ml_error)].
+:- [multlog(ml_lgc)].
+:- [multlog(ml_check)].
+:- [multlog(ml_kernl)].
+:- [multlog(ml_tex)].
+:- [multlog(propres_compact)].
+:- [multlog(ml_minimize)].
+:- [multlog(ml_compactrepr)].
+
+:- (dynamic logName/2, logTVs/2, logDTVs/2, logNDTVs/2, logOp/3).
 
 % loadLogic(+Filename,+Lg) -- load definition of logic Lg from file FileName
 % from FileName
-
-loadLogic(FN,Lg) :-
+loadLogic(FN, Lg) :-
     lgc_in(FN),
     check,
     lgcLN(LN),
@@ -16,148 +32,94 @@ loadLogic(FN,Lg) :-
     assertz(logTVs(Lg, TVs)),
     lgcDTVs(DTVs),
     assertz(logDTVs(Lg, DTVs)),
-    subtract(TVs,DTVs,NDTVs),
+    subtract(TVs, DTVs, NDTVs),
     assertz(logNDTVs(Lg, NDTVs)),
-    (lgcOp(N, _),
-	   chkOp(N, S),
-	   assertz(logOp(Lg, N, S)),
-	   fail
-	;  true
-	),
-    format("Logic ~s loaded as '~a'.", [LN, Lg]),
-    !.
+    (   lgcOp(N, _),
+        chkOp(N, S),
+        assertz(logOp(Lg, N, S)),
+        fail
+    ;   true
+    ),
+    format("Logic ~s loaded as '~a'.", [LN, Lg]), nl, !.
 
 % valueOp(Lg, Op, Arg, Val) -- in logic Lg, the value of Op applied to
 % Arg is V.
-
 valueOp(Lg, Op/Ar, Arg, V) :-
-    logOp(Lg, Op/Ar, Map),
-    member(Arg:V,Map).
+    logOp(Lg, Op/Ar, Map), !,
+    member(Arg:V, Map).
 valueOp(Lg, Op, Arg, V) :-
     logOp(Lg, Op/_, Map), !,
     member(Arg:V, Map).
 
-% valueFmlaT(Lg, F, Val) -- in logic Lg, formula F has value V.
-
-valueFmla(Lg, F, V) :-
-    logTVs(Lg, TVs), !,
-    member(V, TVs),
-    fmlaList(F, L),
-    valueList(Lg, L, V, TVs).
-
-% valueList(Lg, List, Val, TVs) -- in logic Lg, the expression corresponding
-% to List has value Val (TVs is the list of truth values of logic Lg).
-
-valueList(_, TV, TV, TVs) :-
-    member(TV, TVs).
-valueList(Lg, [Op|Args], Val, TVs) :-
-    valueLists(Lg, Args, Vals, TVs),
-    valueOp(Lg, Op, Vals, Val).
-
-valueLists(_, [], [], _).
-valueLists(Lg, [Arg|Args], [Val|Vals], TVs) :-
-    valueList(Lg, Arg, Val, TVs),
-    valueLists(Lg, Args, Vals, TVs).
-
-% fmlaList(F, L) -- recursively convert a formula F to a nested list L.
-
-fmlaList(F, Atom) :-
-    F =.. [Atom], !.
-fmlaList(F, [Op|Lists]) :-
-    F =.. [Op|Args],
-    fmlaLists(Args,Lists).
-
-fmlaLists([],[]) :- !.
-fmlaLists([A|Args], [L|Lists]) :-
-    fmlaList(A,L),
-    fmlaLists(Args,Lists).
-
+% hasValue(Lg, FS, V) -- in logic Lg, formula skeleton FS has value V.
+hasValue(Lg, FS, V) :-
+    logOp(Lg, FS/0, [[]:V]).
+hasValue(Lg, V, V) :-
+    logTVs(Lg, TVs),
+    member(V, TVs).
+hasValue(Lg, FS, V) :-
+    logOp(Lg, Op/A, Map),
+    length(Args, A),
+    FS =.. [Op|Args],
+    maplist(hasValue(Lg), Args, Vals),
+    member(Vals:V, Map).
 
 % isDesignated(Lg, F) -- The value of F in logic Lg is
 % designated. If F contains variables, every value combination for
 % these variables for which F is designated is a solution.
-
 isDesignated(Lg, F) :-
     logTVs(Lg, TVs),
     logDTVs(Lg, DTVs),
     term_variables(F, Vars), !,
-    listOfTVs(Vars,TVs),
-    valueFmla(Lg, F, V),
+    listOfTVs(Vars, TVs),
+    hasValue(Lg, F, V),
     member(V, DTVs).
 
 % isUnDesignated(Lg, F) -- same, except will find all solutions
 % where F has a non-designated value.
-
 isUnDesignated(Lg, F) :-
     logTVs(Lg, TVs),
     logNDTVs(Lg, NDTVs),
     term_variables(F, Vars), !,
-    listOfTVs(Vars,TVs),
-    valueFmla(Lg, F, V),
+    listOfTVs(Vars, TVs),
+    hasValue(Lg, F, V),
     member(V, NDTVs).
 
 % isTaut(Lg, F) -- F is a tautology of Lg.
-
 isTaut(Lg, F) :-
     logTVs(Lg, TVs),
     logDTVs(Lg, DTVs),
     term_variables(F, Vars), !,
-    forall(listOfTVs(Vars,TVs),
-        (   valueFmla(Lg, F, V),
-            member(V, DTVs))).
+    forall(listOfTVs(Vars, TVs),
+           ( hasValue(Lg, F, V),
+             member(V, DTVs)
+           )).
 
 % listOfTVs(L, TVs) -- L is a subset of TVs (works as generator of
 % lists of truth values).
-
 listOfTVs([], _) :- !.
 listOfTVs([L|Ls], TVs) :-
     member(L, TVs),
-    listOfTVs(Ls,TVs).
+    listOfTVs(Ls, TVs).
 
-% Attempts at making a generator for formulas
+% F is a formula skeleton of length N in logic Lg, with variables Vs.
+isFmla(Lg, F, N, Vs) :-
+    length(S, N),
+    isFmla(Lg, F, S, [], Vs, []).
 
-isFormula(Lg, V) :-
-    var(V).
-isFormula(Lg, F) :-
-    logOp(Lg,Op/N,_),
-    functor(F, Op, N),
-    F =.. [Op|Args],
-    areFormulas(Lg, Args).
-
-areFormulas(_, []).
-areFormulas(Lg, [F|Fs]) :-
-    isFormula(Lg, F),
-    areFormulas(Lg, Fs).
-
-
-% fmlaListDepth
-
-fmlaListDepth(_, [V], 1) :-
-    var(V).
-fmlaListDepth(Lg, [Op|Args], D) :-
-    logOp(Lg, Op/Ar, _),
-    length(Args, Ar),
-    fmlaListsDepth(Lg, Args, Ds), D is Ds+1.
-
-fmlaListsDepth(_, [], 0) :- !.
-fmlaListsDepth(Lg, [L|Ls], M) :-
-    fmlaListDepth(Lg, L, D),
-    fmlaListsDepth(Lg, Ls, N), M is D+N.
-
-fmlaPolish(Lg, F, [F]) :-
+% isFmla(Lg, F, S, S0, Vs, Vs0)
+% F is a formula skeleton of size S-S0, with variables Vs-Vs0
+isFmla(_Lg, F, [_|S], S, [F|Vs], Vs) :-
     var(F).
-fmlaPolish(Lg, Op, [Op/0]) :-
-    logOp(Lg, Op/0, _).
-fmlaPolish(Lg, F, [Op/N|PArgs]) :-
-    logOp(Lg, Op/N, _), !,
-    fmlasPolish(Lg, Args, PArgs),
-    length(Args, N),
-    F =.. [Op|Args].
+isFmla(Lg, F, [_|S], S0, Vs, Vs0) :-
+    logOp(Lg, Op/Ar, _),
+    functor(F, Op, Ar),
+    F =.. [_|Fs],
+    isFmla_l(Lg, Fs, S, S0, Vs, Vs0).
 
-fmlasPolish(_, [], []).
-fmlasPolish(Lg,[F],List) :-
-    fmlaPolish(Lg,F,List).
-fmlasPolish(Lg,[F|Fs],List) :-
-    fmlaPolish(Lg,F,L1),
-    fmlasPolish(Lg,Fs,L2),
-    append(L1,L2,List).
+% isFmla_l(Lg, Fs, S, S0, Vs, Vs0)
+% Fs is a list of formulas with total size S-S0 and variables Vs-Vs0
+isFmla_l(_Lg, [], S, S, Vs, Vs).
+isFmla_l(Lg, [F|Fs], S, S0, Vs, Vs0) :-
+    isFmla(Lg, F, S, S1, Vs, Vs1),
+    isFmla_l(Lg, Fs, S1, S0, Vs1, Vs0).
